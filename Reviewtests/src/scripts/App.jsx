8c3b6a74e1e6736538afdb8c60a2dacfa40f498a
Aniket Chowdhury <aniket_chowdhury@hotmail.com>
2020-07-31 09:48:28 +0530

    feat: pull from rt

--- Reviewtests/src/scripts/App.jsx
+++ Reviewtests/src/scripts/App.jsx
@@ -1,14 +1,17 @@
 import React from 'react'
 import '../styles/App.scss'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import Main from './Main.jsx'
 import Thankyou from './Thankyou.jsx'
 const App = () => {
 	return (
 		<>
-			<Router>
-				<Route path="/React" component={Main} />
-				<Route path="/Thankyou" component={Thankyou} />
+			<Router basename='app'>
+				<Switch>
+					<Route path='/' component={Main} exact />
+					<Route path='/table' component={Table} />
+					<Route path='/thankyou' component={Thankyou} />
+				</Switch>
 			</Router>
 		</>
 	)
---
+++ Reviewtests/src/scripts/Table.jsx
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+
+import qs from 'query-string'
+
+import options from './config'
+import axios from 'axios'
+
+import '../styles/Table.scss'
+
+export default class Table extends Component {
+	state = { data: [], arr: [], parsed: { qty: 10, page: 1 } }
+	async componentDidMount() {
+		let parsed = qs.parse(this.props.location.search)
+		if (!parsed.page) {
+			parsed.page = 1
+		}
+		if (!parsed.qty) {
+			parsed.qty = 10
+		}
+
+		this.setState({ parsed: parsed })
+
+		let result = await axios.post(options.link + 'list/all', {
+			page: parsed.page,
+			qty: parsed.qty,
+		})
+
+		this.setState({ data: result.data.users, pages: result.data.pages })
+
+		let arr = []
+
+		for (let index = 0; index < result.data.pages; index++) {
+			arr.push(index)
+		}
+
+		this.setState({ arr: arr })
+	}
+
+	render() {
+		return (
+			<React.Fragment>
+				<section id='admin'>
+					Pages:{' '}
+					<span className='pages'>
+						{this.state.arr.map((num) => (
+							<a
+								key={num}
+								href={`?page=${num + 1}&qty=${
+									this.state.parsed.qty
+								}`}
+							>
+								{num + 1}
+							</a>
+						))}
+					</span>
+					<br/>
+					Number on a page:
+					<span className='pages'>
+						<a href={`?qty=10&page=${this.state.parsed.page}`}>10</a>
+						<a href={`?qty=50&page=${this.state.parsed.page}`}>50</a>
+						<a href={`?qty=100&page=${this.state.parsed.page}`}>100</a>
+					</span>
+					<table>
+						<thead>
+							<tr>
+								<th>created_at</th>
+								<th>email</th>
+								<th>first</th>
+								<th>gender</th>
+								<th>iam</th>
+								<th>language</th>
+								<th>last</th>
+								<th>phone</th>
+								<th>qualification</th>
+								<th>test_data</th>
+								<th>title</th>
+								<th>course_id.stream</th>
+								<th>course_id.discipline</th>
+								<th>course_id.course_name</th>
+								<th>updated_at</th>
+								<th>user_type</th>
+							</tr>
+						</thead>
+						<tbody>
+							{this.state.data.map((elem) => {
+								return (
+									<React.Fragment key={elem._id}>
+										<tr>
+											<td>{elem.created_at}</td>
+											<td>{elem.email}</td>
+											<td>{elem.first}</td>
+											<td>{elem.gender}</td>
+											<td>{elem.iam}</td>
+											<td>{elem.language}</td>
+											<td>{elem.last}</td>
+											<td>{elem.phone}</td>
+											<td>{elem.qualification}</td>
+											<td>{elem.test_data}</td>
+											<td>{elem.title}</td>
+											<td>{elem.course_id.stream}</td>
+											<td>{elem.course_id.discipline}</td>
+											<td>
+												{elem.course_id.course_name}
+											</td>
+											<td>{elem.updated_at}</td>
+											<td>{elem.user_type}</td>
+										</tr>
+									</React.Fragment>
+								)
+							})}
+						</tbody>
+					</table>
+				</section>
+			</React.Fragment>
+		)
+	}
+}
